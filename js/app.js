5f3a9301649ddd0eedb55b00b0006fb9ad46e66f
Denis Tsebin <tsebin.denis@yandex.ru>
2017-07-14 23:43:28 +0300

    Start write

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var AddBook = React.createClass({
+  render: function() {
+    return (
+      <div className="addBook">
+        <form>
+          
+        </form>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
